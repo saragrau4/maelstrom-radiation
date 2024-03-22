78e3c47e8911586f13f28b1dd956a22067b642e8
saragrau4 <138492377+saragrau4@users.noreply.github.com>
2024-03-22 10:27:49 +0100

    Update run_main.py

--- mlproject/run_main.py
+++ mlproject/run_main.py
@@ -6,24 +6,25 @@ import numpy as np
 from climetlab_maelstrom_radiation.benchmarks.utils import EpochTimingCallback
 from climetlab_maelstrom_radiation.benchmarks.metrics_callback import MetricsCallback
 
-X_train = np.random.rand(100, 10)
-y_train = np.random.rand(100, 1)
-
-X_val = np.random.rand(20, 10)
-y_val = np.random.rand(20, 1)
-
-model = Sequential([
-    Dense(64, activation='relu', input_shape=(10,)),
-    Dense(1)
-])
-
-model.compile(optimizer='adam',
-              loss='mse',
-              metrics=['mae', 'mse'])
-
-
-with mlflow.start_run():
-    model.fit(X_train, y_train, epochs=5, validation_data=(X_val, y_val), callbacks=[MetricsCallback(), EpochTimingCallback()])
+def train_model(epochs):
+    X_train = np.random.rand(100, 10)
+    y_train = np.random.rand(100, 1)
+    
+    X_val = np.random.rand(20, 10)
+    y_val = np.random.rand(20, 1)
+    
+    model = Sequential([
+        Dense(64, activation='relu', input_shape=(10,)),
+        Dense(1)
+    ])
+    
+    model.compile(optimizer='adam',
+                  loss='mse',
+                  metrics=['mae', 'mse'])
+    
+    
+    with mlflow.start_run():
+        model.fit(X_train, y_train, epochs=5, validation_data=(X_val, y_val), callbacks=[MetricsCallback(), EpochTimingCallback()])
 
 def main():
     parser = argparse.ArgumentParser(description="Train a simple Keras model and log metrics with MLflow.")
